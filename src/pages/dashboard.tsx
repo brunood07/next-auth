bfa73a4bcaecee6f73be31ede5d6fbe2326599bf
Bruno Domingues <brunood07@gmail.com>
2022-01-19 12:17:52 -0300

    concluído

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,12 +1,13 @@
 import { useContext, useEffect } from "react";
+
+import { Can } from "../components/can";
 import { AuthContext } from "../contexts/AuthContext";
 import { setupAPIClient } from "../services/api";
 import { api } from "../services/apiClient";
-
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext);
+  const { user, signOut } = useContext(AuthContext);
 
   useEffect(() => {
     api.get("/me")
@@ -15,15 +16,22 @@ export default function Dashboard() {
   }, []);
 
   return ( 
-    <h1>Dashboard: {user?.email}</h1>
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+
+      <button onClick={signOut}>Sign out</button>
+    
+      <Can permissions={["metrics.list"]}>
+        <div>Métricas</div>
+      </Can>
+    </>
   );
 }
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
   const apiClient = setupAPIClient(ctx);
+  const response = await apiClient.get("/me");
   
-  const response = await api.get("/me");
-
   return {
     props: {}
   }
